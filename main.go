6696e452d2677e6340f743fc5d568f69bd46e932
James Buckle <james.buckle.23@um.edu.mt>
2026-05-15 16:28:41 +0200

    ..

--- main.go
+++ main.go
@@ -13,24 +13,6 @@ const development = true
 func main() {
 	if development {
 		p := `
-			// type nat = +{zero : 1, succ : nat}
-			// def create_zero() : nat @ 0 = t: 1 <- new close self; self.zero<t>
-			// def a_body() : nat * 1 @ 0 = z0: nat <- new (create_zero()); term: 1 <- new close self; send self <z0, term>
-			// def b_body() : 1 * 1 @ 0 = term1: 1 <- new close self; term2: 1 <- new close self; send self <term1, term2> // to be used for checking type mismatch
-			// def new_body(): nat * 1 @ 0 = a <- spawn (a_body()); y <- sync <a>; <z, y'> <- recv y; wait y'; term: 1 <-new close self; send self <z, term>
-			// prc[a] : nat * 1 @ 1 = w <- new (new_body()); <z, w'> <- recv w; wait w'; term: 1 <-new close self; send self <z, term>
-			// prc[a] : nat * 1 @ 7 = term:1 <- new close self; let x = 5 in send self <x, term>
-
-			// to test SNDTOSYNCED, also tests RCVFROMSYNCED and IGNORE
-			// def body() : nat -* (nat * 1) @ 0 = <x, y> <- recv self; term:1 <- new close self; send y <x, term>
-			// prc[q] : nat * 1 @ 1 = a <- spawn (body()); b <- spawn (body()); y <- sync <a, b>; let x = 5 in send y <x, self>
-
-			// to test RCVFROMSYNCED and IGNORE
-			// def body() : nat * 1 @ 0 = term:1 <- new close self;  let x = 5 in send self <x, term>
-			// prc[q] : nat * 1 @ 1 = a <- spawn (body()); b <- spawn (body()); y <- sync <a, b>; <x, t> <- recv y; send self <x, t>
-
-			// to test CLSSYNCED1 and CLSSYNCED2
-			// N.B. term and prc[q] will me marked as live processes by the end of the running, since they have no receiving client
 			def body() : nat -* (nat * 1) @ 0 = c:1 <- new close self; <x, y> <- recv self; send y <x, c>
 			def send_to_repl(x : nat -* (nat * 1)): nat * 1 @ 1 = let v = 5 in send x <v, self>
 			prc[q] : nat * 1 @ 1 = a <- spawn (body()); b <- spawn (body()); y <- sync <a, b>; z <- new send_to_repl(y); <x, t> <- recv z; wait t; term:1 <- new close self; send self <x, term>
